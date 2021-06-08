01f6e941b6fcbe67ec50a18b970431a4f6ccd096
Julia Aasmaa <my_e_10@hotmail.com>
2021-06-08 10:14:09 +0300

    add pets type table list

--- TITpv19CatsAndDogs/Form1.Designer.cs
+++ TITpv19CatsAndDogs/Form1.Designer.cs
@@ -28,13 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.types = new System.Windows.Forms.ListBox();
+            this.names = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // types
+            // 
+            this.types.FormattingEnabled = true;
+            this.types.Location = new System.Drawing.Point(94, 93);
+            this.types.Name = "types";
+            this.types.Size = new System.Drawing.Size(120, 160);
+            this.types.TabIndex = 0;
+            // 
+            // names
+            // 
+            this.names.FormattingEnabled = true;
+            this.names.Location = new System.Drawing.Point(293, 93);
+            this.names.Name = "names";
+            this.names.Size = new System.Drawing.Size(120, 160);
+            this.names.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(94, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Pet Types";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(293, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Pets";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.names);
+            this.Controls.Add(this.types);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox types;
+        private System.Windows.Forms.ListBox names;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- TITpv19CatsAndDogs/Form1.cs
+++ TITpv19CatsAndDogs/Form1.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +14,34 @@ namespace TITpv19CatsAndDogs
 {
     public partial class Form1 : Form
     {
+        string connectionString;
+        SqlConnection connection;
+
         public Form1()
         {
             InitializeComponent();
+            connectionString = ConfigurationManager.ConnectionStrings["TITpv19CatsAndDogs.Properties.Settings.PetsConnectionString"].ConnectionString;
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            PopulatePetTypeTable();
+        }
+
+        private void PopulatePetTypeTable()
+        {
+            using (connection = new SqlConnection(connectionString))
+            using (SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM PetType", connection))
+            {
+                DataTable petTypeTable = new DataTable();
+                adapter.Fill(petTypeTable);
+
+                types.DisplayMember = "PetTypeName";
+                types.ValueMember = "Id";
+                types.DataSource = petTypeTable;
+        
+            }
+        }
+
     }
 }
